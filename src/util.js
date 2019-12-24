61fafb11e33e661c2489def1835eacf3f0befc56
Craig Jacobson <craig.crj@gmail.com>
2019-12-24 16:27:49 -0700

    Refined some logging functions.

--- src/util.js
+++ src/util.js
@@ -3,29 +3,61 @@
  * @author Craig Jacobson
  */
 /* Core */
+const process = require('process');
 /* Community */
 /* Custom */
 'use strict';
 
-
-const debug = function(msg) {
-  if (!msg) {
-    msg = '';
+function getLine(errStack) {
+  try {
+    return errStack.stack.split('at ')[2].trim();
   }
-  const logLineDetails = (new Error().stack).split('at ')[3].trim();
-  console.log('DEBUG', new Date().toUTCString(), logLineDetails, msg);
-};
+  catch (err) {
+    return 'no details';
+  }
+}
 
 const trace = function(msg) {
   if (!msg) {
     msg = '';
   }
-  const logLineDetails = (new Error().stack).split('at ')[3].trim();
-  console.log('TRACE', new Date().toUTCString(), logLineDetails, msg);
+  console.log('TRACE', new Date().toUTCString(), getLine(new Error()), msg);
+};
+
+const debug = function(msg) {
+  if (!msg) {
+    msg = '';
+  }
+  console.log('DEBUG', new Date().toUTCString(), getLine(new Error()), msg);
+};
+
+const info = function(msg) {
+  if (!msg) {
+    msg = '';
+  }
+  console.log('INFO_', new Date().toUTCString(), getLine(new Error()), msg);
+};
+
+const warn = function(msg) {
+  if (!msg) {
+    msg = '';
+  }
+  console.log('WARN!', new Date().toUTCString(), getLine(new Error()), msg);
+};
+
+const crit = function(msg) {
+  if (!msg) {
+    msg = '';
+  }
+  console.log('CRIT!', new Date().toUTCString(), getLine(new Error()), msg);
+  process.exit(1);
 };
 
 module.exports = {
-  debug,
   trace,
+  debug,
+  info,
+  warn,
+  crit,
 };
 
