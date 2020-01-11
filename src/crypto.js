a7129780120b188793a25fadf241f6c71a3de6b0
Craig Jacobson <craig.crj@gmail.com>
2020-01-11 13:36:23 -0700

    Added signature functions.

--- src/crypto.js
+++ src/crypto.js
@@ -8,15 +8,17 @@
 /* Community */
 const sodium = require('sodium-native');
 /* Custom */
+const log = require('./log.js');
 'use strict';
 
 
 /**
  * Create a random identifier that fits in UInt32 space.
- * @param {Buffer} buf - Optional. Buffer to store random bytes until they are read.
  * @return {Number} Random number.
  */
-function mkId(buf) {
+function mkId() {
+  return sodium.randombytes_random();
+  /*
   if (!buf || buf.length < 4) {
     buf = Buffer.allocUnsafe(4);
   }
@@ -24,6 +26,7 @@ function mkId(buf) {
   sodium.randombytes_buf(buf);
 
   return buf.readUInt32LE(0);
+  */
 }
 
 const NONCE_BYTES = sodium.crypto_secretbox_NONCEBYTES;
@@ -63,10 +66,7 @@ function mkKeyPair(publicKey, secretKey) {
 
   sodium.crypto_box_keypair(publicKey, secretKey);
 
-  return {
-    publicKey,
-    secretKey,
-  };
+  return { publicKey, secretKey };
 }
 
 const BOX_MAC_BYTES = sodium.crypto_box_MACBYTES;
@@ -86,6 +86,7 @@ function box(emessage, message, nonce, publicKey, secretKey) {
     return true;
   }
   catch (err) {
+    log.warn('Failed to box message:', err);
     return false;
   }
 }
@@ -118,6 +119,7 @@ function seal(emessage, message, publicKey) {
     return true;
   }
   catch (err) {
+    log.warn('Failed to seal message:', err);
     return false;
   }
 }
@@ -174,22 +176,81 @@ function verifyHash(message, hash) {
   return 0 === test.compare(h);
 }
 
+const SEED_BYTES = sodium.crypto_sign_SEEDBYTES;
+const SIGN_MAC_BYTES = sodium.crypto_sign_BYTES;
+const SIGN_PUBLIC_BYTES = sodium.crypto_sign_PUBLICKEYBYTES;
+const SIGN_SECRET_BYTES = sodium.crypto_sign_SECRETKEYBYTES;
+
+/**
+ * Create a key pair for signing messages.
+ * @return {Object} Has publicKey {Buffer} and secretKey {Buffer}.
+ */
+function mkSignPair(seed) {
+  const publicKey = Buffer.allocUnsafeSlow(SIGN_PUBLIC_BYTES);
+  const secretKey = Buffer.allocUnsafeSlow(SIGN_SECRET_BYTES);
+  if (!seed || seed.length !== SEED_BYTES) {
+    sodium.crypto_sign_keypair(publicKey, secretKey);
+  }
+  else {
+    sodium.crypto_sign_seed_keypair(publicKey, secretKey, seed);
+  }
+  return { publicKey, secretKey };
+}
+
+/**
+ * Sign the message.
+ * @param {Buffer} signature - Where to put the signature.
+ * @param {Buffer} message - What to sign.
+ * @param {Buffer} secretKey - What to sign with.
+ * @return {Buffer} Signature.
+ */
+function sign(signature, message, secretKey) {
+  try {
+    sodium.crypto_sign_detached(signature, message, secretKey);
+    return true;
+  }
+  catch (err) {
+    log.warn('Failed to sign message:', err);
+    return false;
+  }
+}
+
+/**
+ * Verify the message was signed by other party.
+ * @return True on successfully verifying signature; false otherwise.
+ */
+function unsign(signature, message, publicKey) {
+  return sodium.crypto_sign_verify_detached(signature, message, publicKey);
+}
+
 module.exports = {
   mkId,
+
+  NO_NONCE,
+  NONCE_BYTES,
   mkNonce,
+
+  NO_KEY,
+  PUBLIC_KEY_BYTES,
   mkKeyPair,
+
+  BOX_MAC_BYTES,
   box,
   unbox,
+
+  SEAL_MAC_BYTES,
   seal,
   unseal,
-  NONCE_BYTES,
-  PUBLIC_KEY_BYTES,
-  BOX_MAC_BYTES,
-  SEAL_MAC_BYTES,
-  NO_NONCE,
-  NO_KEY,
+
   HASH_BYTES,
   mkHash,
   verifyHash,
+
+  SIGN_MAC_BYTES,
+  SEED_BYTES,
+  SIGN_PUBLIC_BYTES,
+  mkSignPair,
+  sign,
+  unsign,
 };
 
